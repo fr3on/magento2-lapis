b3e07c1899b1a39ea0e95ca9b0bfd10dd4dbb6d1
fr3on <ahmed@75bit.com>
2026-04-14 23:39:05 +0200

    fix: ensure global scope availability for UI functions and correct RequireJS paths

--- view/adminhtml/requirejs-config.js
+++ view/adminhtml/requirejs-config.js
@@ -1,7 +1,10 @@
 var config = {
-    map: {
-        '*': {
-            'd3': 'https://d3js.org/d3.v7.min'
+    paths: {
+        'd3': 'https://d3js.org/d3.v7.min'
+    },
+    shim: {
+        'd3': {
+            exports: 'd3'
         }
     }
 };
