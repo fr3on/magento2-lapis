13ab822a7ba8d2f05be18d321838121f5d8882ab
fr3on <ahmed@75bit.com>
2026-04-15 00:16:11 +0200

    fix: convert D3.js into a native Magento AMD module to resolve RequireJS mismatch errors

--- view/adminhtml/requirejs-config.js
+++ view/adminhtml/requirejs-config.js
@@ -1,15 +1,5 @@
 var config = {
-    map: {
-        '*': {
-            'd3': 'Fr3on_Lapis/js/lapis-d3-v7-stable'
-        }
-    },
     paths: {
-        'd3': 'Fr3on_Lapis/js/lapis-d3-v7-stable'
-    },
-    shim: {
-        'd3': {
-            exports: 'd3'
-        }
+        'd3': 'Fr3on_Lapis/js/d3-native'
     }
 };
