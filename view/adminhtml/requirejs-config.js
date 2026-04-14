a67d2789015138faa69d96b46e759fc539d998a9
fr3on <ahmed@75bit.com>
2026-04-14 23:53:00 +0200

    fix: resolve global scope reference errors and stabilize RequireJS mapping

--- view/adminhtml/requirejs-config.js
+++ view/adminhtml/requirejs-config.js
@@ -1,7 +1,10 @@
 var config = {
-    map: {
-        '*': {
-            'd3': 'Fr3on_Lapis/js/d3'
+    paths: {
+        'd3': 'Fr3on_Lapis/js/d3'
+    },
+    shim: {
+        'd3': {
+            exports: 'd3'
         }
     }
 };
